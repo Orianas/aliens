3353d1a05a879d44db314771b892ed3e492f31ef
Steve Gilland <steven.gilland@gmail.com>
2017-04-19 11:32:09 -0500

    Added Main Menu

--- core/src/com/project/alien/screens/GameScreen.java
+++ core/src/com/project/alien/screens/GameScreen.java
@@ -1,5 +1,8 @@
 package com.project.alien.screens;
 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.GL20;
+
 public class GameScreen extends AbstractScreen {
 
     public GameScreen() {
@@ -8,6 +11,16 @@ public class GameScreen extends AbstractScreen {
 
     @Override
     public void buildStage() {
+
+
         // TODO: Add Actors
+    }
+
+    public void render(float delta) {
+        Gdx.gl.glClearColor(0, 1, 0, 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+        super.act(delta);
+        super.draw();
     }
 }
--- core/src/com/project/alien/screens/MainMenuScreen.java
+++ core/src/com/project/alien/screens/MainMenuScreen.java
@@ -1,13 +1,60 @@
 package com.project.alien.screens;
 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
+import com.badlogic.gdx.scenes.scene2d.InputListener;
+import com.badlogic.gdx.scenes.scene2d.ui.Image;
+import com.badlogic.gdx.scenes.scene2d.ui.ImageButton;
+import com.badlogic.gdx.utils.Align;
+import com.project.alien.utils.ScreenEnum;
+import com.project.alien.utils.UIFactory;
+
 public class MainMenuScreen extends AbstractScreen {
+
+    private Texture txtrBg;
+    private Texture txtrNewGame;
+    private Texture txtrExit;
 
     public MainMenuScreen() {
         super();
+        txtrBg = new Texture(Gdx.files.internal("img/MainMenuBG.png"));
+        txtrNewGame = new Texture(Gdx.files.internal("img/newGame.png"));
+        txtrExit = new Texture(Gdx.files.internal("img/exit.png"));
     }
 
     @Override
     public void buildStage() {
-        // TODO: Add Actors
+
+        Image bg = new Image(txtrBg);
+        addActor(bg);
+
+        float midX = getWidth() / 2;
+        float midY = getHeight() / 2;
+
+        ImageButton btnNewGame = UIFactory.createButton(txtrNewGame);
+        btnNewGame.setPosition(midX, midY - 25f, Align.center);
+        addActor(btnNewGame);
+
+        ImageButton btnExit = UIFactory.createButton(txtrExit);
+        btnExit.setPosition(midX, midY - 300f, Align.center);
+        addActor(btnExit);
+
+        btnNewGame.addListener(UIFactory.createListener(ScreenEnum.GAME));
+
+        btnExit.addListener(
+                new InputListener() {
+                    @Override
+                    public boolean touchDown(InputEvent event, float x, float y, int pointer, int button) {
+                        Gdx.app.exit();
+                        return false;
+                    }
+                });
+    }
+
+    @Override
+    public void dispose() {
+        super.dispose();
+        txtrBg.dispose();
     }
 }
