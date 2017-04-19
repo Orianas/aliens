52dc79234952f90b3fad8e0212e2a461fe088549
Steve Gilland <steven.gilland@gmail.com>
2017-04-19 11:32:09 -0500

    Added Main Menu

--- core/src/com/project/alien/utils/Consts.java
+++ core/src/com/project/alien/utils/Consts.java
@@ -4,8 +4,5 @@ import com.badlogic.gdx.math.Vector2;
 
 public class Consts {
 
-    public static final int GAME_HEIGHT = 480;
-    public static final int GAME_WIDTH = 800;
-
     public static final Vector2 WORLD_GRAVITY = new Vector2(0, -10);
 }
