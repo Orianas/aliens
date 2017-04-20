c2e714791fe9f4d37bb2384ffb6d12e5f22fa2ef
CaDougal93 <cade.mcdougal@smail.astate.edu>
2017-04-20 12:02:16 -0500

    Added ZombieCount method in Level class

--- core/src/com/project/alien/utils/Consts.java
+++ core/src/com/project/alien/utils/Consts.java
@@ -6,7 +6,7 @@ import com.badlogic.gdx.math.Vector2;
 public class Consts {
 
     public static final Vector2 WORLD_GRAVITY = new Vector2(0, -10);
-    public static final int RESOURCE_GAIN_AMT = 5;  
+    public static final int RESOURCE_GAIN_AMT = 5;
     public static final float RESOURCE_GAIN_TIME = 3.0f;
     public static final int SCORE_GAIN_AMT = 1;
     public static final float SCORE_GAIN_TIME = 1.5f;
@@ -16,7 +16,5 @@ public class Consts {
     public static final int TOWER_PLOT_3_Y = 190;
     public static final int TOWER_PLOT_4_Y = 260;
     public static final int TOWER_PLOT_5_Y = 330;
-
-
 
 }
