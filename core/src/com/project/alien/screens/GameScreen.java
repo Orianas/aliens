fd5ccb66cd572cc1938c3624a5cd41d16f94e89e
johnreach <john.reach@gmail.com>
2017-04-27 10:39:01 -0500

    Merge branch 'master' of https://github.com/Orianas/aliens into TiledMap
    # Conflicts:
    #	core/src/com/project/alien/screens/GameScreen.java

--- core/src/com/project/alien/screens/GameScreen.java
+++ core/src/com/project/alien/screens/GameScreen.java
@@ -3,37 +3,12 @@ package com.project.alien.screens;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.InputMultiplexer;
 import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.OrthographicCamera;
-import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.BitmapFont;
-import com.badlogic.gdx.scenes.scene2d.Stage;
-import com.badlogic.gdx.scenes.scene2d.ui.Image;
-import com.badlogic.gdx.scenes.scene2d.ui.Label;
-import com.badlogic.gdx.utils.viewport.FitViewport;
 import com.project.alien.Levels.TiledLevel;
-import com.project.alien.utils.Consts;
 import com.project.alien.utils.GameHUD;
 
-import static com.badlogic.gdx.graphics.Color.BLACK;
-import static com.badlogic.gdx.graphics.Color.WHITE;
 
 public class GameScreen extends AbstractScreen {
 
-    private Stage HUD;
-    private Texture txtrHUDBG;
-    private float HUDTIMER;
-    private float SCORETIMER;
-    private float RESOURCETIMER;
-    private int SCORE;
-    private int RESOURCES;
-    private Label currScore;
-    private Label currResources;
-    private Label[] towerCost;
-    private Label labelResources;
-    private Label labelScore;
-    private Label labelTowers;
-    private BitmapFont HUDFont;
-    private Texture txtrCost;
     private GameHUD HUD;
 
 
