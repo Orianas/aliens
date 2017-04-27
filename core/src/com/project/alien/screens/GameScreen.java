1208fb89d7f68989d8fb2500ef460892e8b5c631
Steve Gilland <steven.gilland@gmail.com>
2017-04-27 10:40:46 -0500

    Added pause menu BUGS: click events not working.

--- core/src/com/project/alien/screens/GameScreen.java
+++ core/src/com/project/alien/screens/GameScreen.java
@@ -1,22 +1,30 @@
 package com.project.alien.screens;
 
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
 import com.badlogic.gdx.InputMultiplexer;
 import com.badlogic.gdx.graphics.GL20;
 import com.project.alien.Levels.TiledLevel;
 import com.project.alien.utils.GameHUD;
-
+import com.project.alien.utils.PauseMenu;
+import com.project.alien.utils.State;
 
 public class GameScreen extends AbstractScreen {
 
     private GameHUD HUD;
+    private PauseMenu pauseMenu;
+    private State currState;
 
 
     private TiledLevel level1;
 
     public GameScreen() {
         super();
+        currState = State.Running;
         HUD = new GameHUD();
+        pauseMenu = new PauseMenu();
+        level1 = new Level();
+        level1.create();
         level1 = new TiledLevel("maps/level_1.tmx");
     }
 
@@ -37,6 +45,8 @@ public class GameScreen extends AbstractScreen {
     @Override
     public void buildStage() {
         HUD.buildStage();
+        pauseMenu.buildStage();
+        Gdx.input.setCatchBackKey(true);
     }
 
     @Override
@@ -44,20 +54,40 @@ public class GameScreen extends AbstractScreen {
         Gdx.gl.glClearColor(0, 1, 0, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
-        level1.render();
+        switch (currState) {
+            case Running:
+                level1.render();
 
-        HUD.updateHUD(delta);
+                HUD.updateHUD(delta);
 
-        super.act(delta);
+                super.act(delta);
+                act(delta);
+                HUD.act(delta);
+
+                break;
+            case Paused:
+                pauseMenu.act(delta);
+                pauseMenu.draw();
+                break;
+            default:
+        }
         super.draw();
-        act(delta);
         draw();
-        HUD.HUD.act(delta);
-        HUD.HUD.draw();
+        HUD.draw();
     }
 
     @Override
     public void dispose() {
         super.dispose();
+    }
+
+    @Override
+    public boolean keyDown(int keycode) {
+        if (keycode == Input.Keys.BACK || keycode == Input.Keys.ESCAPE)
+            if (currState == State.Paused)
+                currState = State.Running;
+            else
+                currState = State.Paused;
+        return false;
     }
 }
